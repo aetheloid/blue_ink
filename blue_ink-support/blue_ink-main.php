b7f9a028a72f3043f46122040c1185eddb26ffd3
Nathaniel Thoren <nathaniel.thoren@oehmedwien.local>
2018-11-29 21:13:34 +0100

    began adding widget

---
+++ blue_ink-support/blue_ink-main.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once plugin_dir_path( __FILE__ ) . 'inc/class-blue_ink-support-icons.php'
---
+++ blue_ink-support/blue_ink-support.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Plugin Name: Blue Ink Support
+ * Pugin URI: 
+ * Description: widgets for the Blue Ink theme
+ * Version: 1.0
+ * Author: hmeleon
+ * Author URI:
+ * License: GNU General Public License v3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+if ()
