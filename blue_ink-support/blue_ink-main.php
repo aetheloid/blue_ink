addf0060334b9d5b8452eb7a8cb29b5a7b3bb2c2
Nathaniel Thoren <nathaniel.thoren@oehmedwien.local>
2018-12-10 12:37:31 +0100

    oehpc

--- blue_ink-support/blue_ink-main.php
+++ blue_ink-support/blue_ink-main.php
@@ -1,3 +1,6 @@
 <?php
 
-require_once plugin_dir_path( __FILE__ ) . 'inc/class-blue_ink-support-icons.php'
+require_once plugin_dir_path(__FILE__) . 'inc/class-blue_ink-support-icons.php'
+
+require_once plugin_dir_path(__FILE__) . 'inc/class-blue_ink-support-import-data.php';
+
